edf35dfba9602056f0b14a501604d1a4e6e07c58
Shuai-xh <113590650+Shuai-xh@users.noreply.github.com>
2022-09-15 18:46:15 +0800

    Add files via upload

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx20d9a22636b28c7d",
+  APP_ID: "",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "229ab5d1c57ba7ac658691a7de1206d0",
+  APP_SECRET: "",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -25,9 +25,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "229ab5d1c57ba7ac658691a7de1206d0",
+      id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ebQWJp0kGWWLBeSDFdoHhHQbLWmFULBPoGdihRs7Bek",
+      useTemplateId: "",
       // 所在省份
       province: "广东",
       // 所在城市
